ddc2c6b2fa3a4ea29d63ab93b7547e291de98bed
Sachin <144852966+SuperSachinS@users.noreply.github.com>
2024-02-12 15:34:07 -0700

    DisasterVictim update
    added attribute definitions

--- edu/ucalgary/oop/DisasterVictim.java
+++ edu/ucalgary/oop/DisasterVictim.java
@@ -1,5 +1,38 @@
 package edu.ucalgary.oop;
 
 public class DisasterVictim{
-    
+	
+	//Attributes
+    private String firstName;
+	private String lastName;
+	private String dateOfBirth;
+	private String comments;
+	private static final int ASSIGNED_SOCIAL_ID;
+	private MedicalRecord[] medicalRecords;
+	private FamilyRelation[] familyConnections;
+	private static final String ENTRY_DATE;
+	private Supply[] personalBelongings;
+	private String gender;
+	private int counter;
+	
+	//Methods
+	
+	//Constructors
+	
+	public DisasterVictim(String firstName, String ENTRY_DATE) throws IllegalArgumentException{
+		
+	}
+	//Getters
+	
+	
+	
+	
+	
+	
+	
+	//Setters
+	
+	
+	
+	
 }
