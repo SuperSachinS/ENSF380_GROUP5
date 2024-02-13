70786aac371f0cf950f678dcbbe82d54abd2c509
Julien Rabino <julienmescha.rabino@ucalgary.ca>
2024-02-13 10:18:54 -0700

    finished medicalrecord.java

--- edu/ucalgary/oop/MedicalRecord.java
+++ edu/ucalgary/oop/MedicalRecord.java
@@ -1,5 +1,52 @@
 package edu.ucalgary.oop;
+import java.util.regex.*;
 
 public class MedicalRecord {
+    private Location Location;
+    private String treatmentDetails;
+    private String dateOfTreatment;
+
+    public MedicalRecord(Location location, String treatmentDetails, String dateOfTreatment){
+
+        this.setLocation(location);
+        this.setTreatmentDetails(treatmentDetails);
+        this.setDateOfTreatment(dateOfTreatment);
+    }
+
+    public Location getLocation(){
+        return this.Location;
+    }
+
+    public void setLocation(Location location) throws IllegalArgumentException{
+        if (location == null){
+            throw new IllegalArgumentException("Location cannot be null.");
+        }
+        this.Location = location;
+    }
+
+    public String getTreatmentDetails(){
     
+        return this.treatmentDetails;
+    }
+
+    public void setTreatmentDetails(String details) throws IllegalArgumentException{
+        if (details == null){
+            throw new IllegalArgumentException("Treatment details cannot be null.");
+        }
+        this.treatmentDetails = details;
+    }
+
+    public String getDateOfTreatment(){
+        return this.dateOfTreatment;
+    }
+
+    public void setDateOfTreatment(String date) throws IllegalArgumentException{
+        Pattern datePattern = Pattern.compile("^\\d{4}[-]{1}\\d{2}[-]{1}\\d{2}");
+        Matcher myMatcher = datePattern.matcher(date);
+        if (!myMatcher.find()){
+            throw new IllegalArgumentException("Invalid date");
+
+        }
+        this.dateOfTreatment = date;
+    }
 }
