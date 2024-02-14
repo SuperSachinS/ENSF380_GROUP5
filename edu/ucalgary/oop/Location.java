4d7dbab5c11948927bae0f1cc7cc62e9f90408ba
jin kim <jinkim477@gmail.com>
2024-02-14 11:16:33 -0700

    initial completion of supply and location classes.

--- edu/ucalgary/oop/Location.java
+++ edu/ucalgary/oop/Location.java
@@ -1,5 +1,43 @@
 package edu.ucalgary.oop;
+import java.util.ArrayList;
 
 public class Location {
-    
+    private String name;
+    private String address;
+    private ArrayList<DisasterVictim> occupants;
+    private ArrayList<Supply> supplies;
+
+    public Location(String name, String address){
+        this.setName(name);
+        this.setAddress(address);
+        this.occupants = new ArrayList<DisasterVictim>();
+        this.supplies = new ArrayList<Supply>();
+    }
+
+    public void addOccupant(DisasterVictim occupant){
+        this.occupants.add(occupant);
+    }
+
+    public void removeOccupant(DisasterVictim occupant){
+        this.occupants.remove(occupant);
+    }
+
+    public void addSupply(Supply supply){
+        this.supplies.add(supply);
+    }
+
+    public void removeSupply(Supply supply){
+        this.supplies.remove(supply);
+    }
+
+    public void setName(String name){this.name = name;}
+    public void setAddress(String address){this.address = address;}
+    public void setOccupants(ArrayList<DisasterVictim> occupants){this.occupants = occupants;}
+    public void setSupplies(ArrayList<Supply> supplies){this.supplies = supplies;}
+
+    public String getName(){return this.name;}
+    public String getAddress(){return this.address;}
+    public ArrayList<DisasterVictim> getOccupants(){return this.occupants;}
+    public ArrayList<Supply> getSupplies(){return this.supplies;}
+
 }
