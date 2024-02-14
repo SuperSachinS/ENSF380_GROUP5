6337a0cbd2de81d317fd50ee5c29128cddae7db5
Sachin <144852966+SuperSachinS@users.noreply.github.com>
2024-02-14 09:17:28 -0700

    setter function outlines
    added the framework for all the setter functions

--- edu/ucalgary/oop/DisasterVictim.java
+++ edu/ucalgary/oop/DisasterVictim.java
@@ -1,4 +1,8 @@
+
+
+
 package edu.ucalgary.oop;
+import java.util.regex.*;
 
 public class DisasterVictim{
 	
@@ -37,7 +41,43 @@ public class DisasterVictim{
 	
 	//Setters
 	
+	public void setFirstName(String firstName){
+		this.firstName = firstName;
+	}
 	
+	public void setLastName(String lastName){
+		this.lastName = lastName;
+	}
 	
+	public void setDateOfBirth(String dateOfBirth) throws IllegalArgumentException{
+		//TODO
+	}
 	
+	public void setComments(String comments){
+		this.comments = comments;
+	}
+	
+	public void setMedicalRecords(MedicalRecord[] medicalRecords){
+		//TODO
+	}
+	
+	public void setPersonalBelongings(Supply[] supplies){
+		//TODO
+	}
+	
+	public void setFamilyConnections(FamilyRelation[] relation){
+		//TODO
+	}
+	
+	public void setGender(String gender){
+		this.gender = gender;
+	}
+	
+	public void addPersonalBelonging(Supply supply){
+		//TODO
+	}
+	
+	public void removedPersonalBelonging(Supply supply){
+		
+	}
 }
