74c225ba57190d3838e8a877f547574659947b15
Austin Kim <austinkimkimkim@gmail.com>
2024-02-15 13:21:43 -0700

    Finished inquirer

--- edu/ucalgary/oop/Inquirer.java
+++ edu/ucalgary/oop/Inquirer.java
@@ -1,19 +1,19 @@
 package edu.ucalgary.oop;
 
 public class Inquirer {
-    private static String FIRST_NAME;
-    private static String LAST_NAME;
-    private static String INFO;
-    private static String SERVICES_PHONE;
+    private final String FIRST_NAME;
+    private final String LAST_NAME;
+    private final String INFO;
+    private final String SERVICES_PHONE;
     
     public Inquirer(String FIRST_NAME, String LAST_NAME, String SERVICES_PHONE, String INFO){
-      Inquirer.FIRST_NAME = FIRST_NAME;
-      Inquirer.LAST_NAME = LAST_NAME;
-      Inquirer.SERVICES_PHONE = SERVICES_PHONE;
-      Inquirer.INFO = INFO;
+      this.FIRST_NAME = FIRST_NAME;
+      this.LAST_NAME = LAST_NAME;
+      this.SERVICES_PHONE = SERVICES_PHONE;
+      this.INFO = INFO;
     }
-    public String getFirstName(){return Inquirer.FIRST_NAME;}
-    public String getLastName(){return Inquirer.LAST_NAME;}
-    public String getInfo(){return Inquirer.INFO;}
-    public String getServicesPhone(){return Inquirer.SERVICES_PHONE;}
+    public String getFirstName(){return this.FIRST_NAME;}
+    public String getLastName(){return this.LAST_NAME;}
+    public String getInfo(){return this.INFO;}
+    public String getServicesPhone(){return this.SERVICES_PHONE;}
 }
